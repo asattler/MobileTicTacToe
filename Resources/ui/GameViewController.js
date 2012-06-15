7c2972f18463bb89cb44f38716e0178ee9442664
Benjamin Kuhn <b.kuhn@hotmail.de>
2012-06-15 12:59:42 +0200

    added defensive ai

--- Resources/ui/GameViewController.js
+++ Resources/ui/GameViewController.js
@@ -5,17 +5,17 @@ Ti.API.info('Including GameViewController.js');
 	var PlayerOne,PlayerTwo;
 	
 	var initPlayers = function(){
-		Ti.API.info(mttt.app.svc.settings);
+		var settings = mttt.app.svc.settings;
 		PlayerOne = {
-			name  : mttt.app.svc.settings.player1.begin?mttt.app.svc.settings.player1.name:mttt.app.svc.settings.player2.name,
-	        image : mttt.app.svc.settings.player1.begin?mttt.app.svc.settings.player1.image:mttt.app.svc.settings.player2.image,
-			image_win   : mttt.app.svc.settings.player1.begin?mttt.app.svc.settings.player1.image_win:mttt.app.svc.settings.player2.image_win,
+			name  : settings.player1.begin?settings.player1.name:settings.player2.name,
+	        image : settings.player1.begin?settings.player1.image:settings.player2.image,
+			image_win   : settings.player1.begin?settings.player1.image_win:settings.player2.image_win,
 		};
 		
 		PlayerTwo = {
-			name  : mttt.app.svc.settings.player1.begin?mttt.app.svc.settings.player2.name:mttt.app.svc.settings.player1.name,
-	        image : mttt.app.svc.settings.player1.begin?mttt.app.svc.settings.player2.image:mttt.app.svc.settings.player1.image,
-			image_win   : mttt.app.svc.settings.player1.begin?mttt.app.svc.settings.player2.image_win:mttt.app.svc.settings.player1.image_win,
+			name  : settings.player1.begin?settings.player2.name:settings.player1.name,
+	        image : settings.player1.begin?settings.player2.image:settings.player1.image,
+			image_win   : settings.player1.begin?settings.player2.image_win:settings.player1.image_win,
 		};
 	}
 	var attachToTab = function(_tab) {
@@ -31,32 +31,102 @@ Ti.API.info('Including GameViewController.js');
 				
 		oneWindow.add(this.view);
 	};
-
-	var handleClick = function(_e) {
+	
+	var allocateButtonWithCurrentPlayer = function(index,gvc){
+		var buttons = gvc.buttons;
+		var currentPlayer = gvc.currentPlayer;
+		buttons[index-1].backgroundImage = currentPlayer.image;
+		buttons[index-1].backgroundDisabledImage = currentPlayer.image;
+		buttons[index-1].enabled = false;
+		buttons[index-1].owner = currentPlayer;
+	}
+	
+	var changeCurrentPlayer = function(gvc){
+		gvc.currentPlayer = gvc.turn%2?PlayerOne:PlayerTwo;
+	}
+	
+	var prepareNextTurn = function(gvc){
+		gvc.turn++;
+		setPlayerTurnText(gvc.currentPlayer,gvc);
+	}
+	
+	var handleClick = function(_e,options) {
 		var index = _e.source.index;
 		
-		mttt.app.gvc.buttons[index-1].backgroundImage = mttt.app.gvc.currentPlayer.image;
-		mttt.app.gvc.buttons[index-1].backgroundDisabledImage = mttt.app.gvc.currentPlayer.image;
-		mttt.app.gvc.buttons[index-1].enabled = false;
-		mttt.app.gvc.buttons[index-1].owner = mttt.app.gvc.currentPlayer;
+		var gvc = mttt.app.gvc || options.gvc;
 		
-		var win = checkForWin();
+		allocateButtonWithCurrentPlayer(index,gvc);
+
+		var win = checkForWin(gvc);
 		if(win != null){
 			gameEnd(win);
 		}else {
-			if(mttt.app.gvc.turn%2){
-				mttt.app.gvc.currentPlayer = PlayerOne;
-			}
-			else{
-	            mttt.app.gvc.currentPlayer = PlayerTwo;
-			}
-			mttt.app.gvc.turn++;
-			setPlayerTurnText(mttt.app.gvc.currentPlayer);
+			changeCurrentPlayer(gvc);
+			prepareNextTurn(gvc);	
 		}
 		
-		if(mttt.app.gvc.turn > 8){
+		if(gvc.turn > 8){
 			gameEnd(null);	
 		}
+		
+		if(mttt.app.svc.settings.mode=='single' && gvc.currentPlayer.name == 'cpu'){
+			var btn = checkForWinningChance(gvc);
+			if(btn){
+				// press button to win or to prevent losing
+				pressButton(btn);
+			}
+			else{
+				var lines = gvc.lines;
+				var btns = gvc.buttons;
+				// press first empty field
+				for(x=0;x < lines.length; x++){
+					var index1 = lines[x][0];
+					var index2 = lines[x][1];
+					var index3 = lines[x][2];
+			
+					if(btns[index1-1].owner == null){
+						pressButton(btns[index1-1]);
+						break;
+					}else if(btns[index2-1].owner == null){
+						pressButton(btns[index2-1]);
+						break;
+					}else if(btns[index3-1].owner == null){
+						pressButton(btns[index3-1]);
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	var pressButton = function(btn){
+		if(btn!=null){
+			btn.fireEvent('click',{source:{index:btn.index}});
+		}
+	}
+	
+	var checkForWinningChance = function(gvc){
+		var currentPlayer = gvc.currentPlayer;
+		var lines = gvc.lines;
+		alert(lines);
+		var btns = gvc.buttons;
+		var x;
+		for(x=0;x < lines.length; x++){
+			var index1 = lines[x][0];
+			var index2 = lines[x][1];
+			var index3 = lines[x][2];
+			
+			if(btns[index1-1].owner!=null && btns[index2-1].owner!=null && btns[index1-1].owner.name == btns[index2-1].owner.name && btns[index3-1].owner == null){
+				return btns[index3-1];
+			}
+			else if(btns[index1-1].owner!=null && btns[index3-1].owner!=null && btns[index1-1].owner.name == btns[index3-1].owner.name && btns[index2-1].owner == null){
+				return btns[index2-1];
+			}
+			else if(btns[index3-1].owner!=null && btns[index2-1].owner!=null && btns[index2-1].owner.name == btns[index3-1].owner.name && btns[index1-1].owner == null){
+				return btns[index1-1];
+			}
+		}
+		return null;
 	}
 	
 	var gameEnd = function(win){
@@ -82,17 +152,15 @@ Ti.API.info('Including GameViewController.js');
 		    resetGame();
 		}, 2000);
 		
-		
-		
 	}
 	
-	var setStatusText = function(text){
-		mttt.app.gvc.endLabel.text = text;
+	var setStatusText = function(text,gvc){
+		gvc.endLabel.text = text;
 	};
 	
-	var checkForWin = function(){
-		var lines = mttt.app.gvc.lines;
-		var btns = mttt.app.gvc.buttons;
+	var checkForWin = function(gvc){
+		var lines = gvc.lines;
+		var btns = gvc.buttons;
 		var x;
 		for(x=0;x < lines.length; x++){
 			var index1 = lines[x][0];
@@ -101,35 +169,37 @@ Ti.API.info('Including GameViewController.js');
 			if(btns[index1-1].owner == null || btns[index2-1].owner == null || btns[index3-1].owner == null){ 
 				continue;
 			}
-			if(btns[index1-1].owner == btns[index2-1].owner && btns[index2-1].owner == btns[index3-1].owner){
+			if(btns[index1-1].owner.name == btns[index2-1].owner.name && btns[index2-1].owner.name == btns[index3-1].owner.name){
 				return {line:lines[x], winner:btns[index1-1].owner};
 			}
 		}
 		return null;		
 	}
 	
-	var resetGame = function(){
-		mttt.app.gvc.turn = 0;
-		for (var i=0;i < mttt.app.gvc.buttons.length;i++){
-			var btn = mttt.app.gvc.buttons[i];
+	var resetGame = function(options){
+		var gvc = mttt.app.gvc || options.gvc;
+		gvc.turn = 0;
+		
+		for (var i=0;i < gvc.buttons.length;i++){
+			var btn = gvc.buttons[i];
 			btn.backgroundImage = '/images/leer.png';
 			btn.backgroundDisabledImage = '/images/leer.png';
 			btn.enabled = true;
 			btn.owner = null;
 		}
-		mttt.app.gvc.currentPlayer = PlayerOne;
-		setPlayerTurnText(mttt.app.gvc.currentPlayer);
+		gvc.currentPlayer = PlayerOne;
+		setPlayerTurnText(gvc.currentPlayer,gvc);
+		
+		initPlayers();
 		
 		// KI
-		initPlayers();
 		if(mttt.app.svc.settings.mode=='single' && PlayerOne.name == 'cpu'){
-			Ti.API.info(mttt.app.gvc.buttons);
-			mttt.app.gvc.buttons[0].fireEvent('click');
+			gvc.buttons[0].fireEvent('click',{source:{index:1}});
 		}
 	}
 	
-	var setPlayerTurnText = function(player){
-		setStatusText(player.name+"S TURN");
+	var setPlayerTurnText = function(player,gvc){
+		setStatusText(player.name+"S TURN",gvc);
 	}
 	
   	mttt.ui.createGameViewController = function(_args) {
@@ -168,13 +238,10 @@ Ti.API.info('Including GameViewController.js');
 			for (var y = 0; y < 3; y++) {
 				var index =  1+(x*3)+y;
 				var oneButton = Ti.UI.createButton({
-					backgroundImage: '/images/leer.png',
-					backgroundDisabledImage: '/images/leer.png',
 					width: mttt.config.buttonWidth,
 					height: mttt.config.buttonHeight,
 					center: {x: buttonBaseX + (x*buttonDelta), y: buttonBaseY + (y*buttonDelta)},
 					enabled: true,
-//					gvc: gvc,
 					index: index
 				});
 				buttons.push(oneButton);
@@ -195,17 +262,15 @@ Ti.API.info('Including GameViewController.js');
 		
 		view.add(endLabel);
 		
-		// KI
-		if(mttt.app.svc.settings.mode=='single' && PlayerOne.name == 'cpu'){
-			buttons[0].fireEvent('click');
-		}
-		
 //		var label = Ti.UI.createLabel({text:_args});
 		gvc.lines = lines;
 		gvc.buttons = buttons;
 		gvc.endLabel = endLabel;
 		gvc.view = view;
 		gvc.resetGame = resetGame;
+		
+		resetGame({gvc:gvc});		
+		
 	    return gvc;
 	};
 
