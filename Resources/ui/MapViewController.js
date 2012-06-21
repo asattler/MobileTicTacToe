ff4a15816ada303c06040fdf95585b71476d15a7
Andreas Sattler <info@royal-scripts.de>
2012-06-21 19:31:41 +0200

    problem with crash while click on annotation is solved. icons added for playing + info

--- Resources/ui/MapViewController.js
+++ Resources/ui/MapViewController.js
@@ -20,25 +20,42 @@ Ti.API.info('Including SettingsViewController.js');
 		
 		var mapview = Titanium.Map.createView({
             mapType: Titanium.Map.STANDARD_TYPE,
-            region: {latitude: 52.545226,
-            longitude: 13.351818,
-            latitudeDelta:0.01, 
-            longitudeDelta:0.01},
+            region: {
+            	latitude: 52.545226,
+            	longitude: 13.351818,
+            	latitudeDelta:0.01, 
+            	longitudeDelta:0.01
+            },
             animate:true,
             regionFit:true,
             userLocation:true,
+    		visible: true,
         });
 
 		var myAnnotation = Titanium.Map.createAnnotation({
 			draggable : false,
+			pincolor : Titanium.Map.ANNOTATION_RED,
 			latitude : 52.543855,
-			longitude : 13.35538
+			longitude : 13.35538,
+			title : "Herbert Husten",
+			subtitle : "Score 12",
+			leftButton : "/images/play.png",
+			rightButton : "/images/next.png",
 		});
 		
 		//click-event is not supported for annotations on android...
 		mapview.addEventListener("click", function(e){
-			alert("play with me");
-		})
+			//alert(e.clicksource);
+			if(e.clicksource === "leftPane"){
+				alert("Play with me");
+			}
+			else if(e.clicksource === "rightPane"){
+				alert("Info");
+			}
+			//mapview.showAnnotation(e.annotation);
+
+			//alert("play with me");
+		});
 
 		mapview.addAnnotation(myAnnotation);
 
